0adf316961ac530d7ba7d31b5626ab5d78e5d728
Tarasish bhattacharya <tarasishbhattacharya06@gmail.com>
2022-06-07 21:00:27 +0530

    f

---
+++ src/App.js
@@ -0,0 +1,13 @@
+// import logo from './logo.svg';
+import './App.css';
+import { Displayhotel } from './components/Displayhotel';
+
+function App() {
+  return (
+    <div >
+      <Displayhotel/>
+    </div>
+  );
+}
+
+export default App;
